2bd815d13a91b09cab36099ecb0a83c8d82c2b49
vickipetrova <victoria.petrova@uni.minerva.edu>
2024-06-24 09:14:54 +0300

    Initial Commit

---
+++ memorizegame/memorizegameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizegameApp.swift
+//  memorizegame
+//
+//  Created by Victoria Petrova on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct memorizegameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
